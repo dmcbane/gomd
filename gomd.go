1971b308f29c63d43712be91a820f399538de33d
H. Dale McBane <h.dale.mcbane@gmail.com>
2020-02-23 23:00:27 -0500

    builds with latest version os echo but not functional

--- gomd.go
+++ gomd.go
@@ -13,32 +13,31 @@ import (
 
 	"github.com/GeertJohan/go.rice"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/standard"
 	"github.com/labstack/echo/middleware"
 	"github.com/nochso/gomd/eol"
 	"github.com/toqueteos/webbrowser"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-type InputArgs struct {
+type inputArgs struct {
 	Port *int
 	File *string
 }
 
-var args = InputArgs{
+var args = inputArgs{
 	Port: kingpin.Flag("port", "Listening port used by webserver").Short('p').Default("1110").Int(),
 	File: kingpin.Arg("file", "Markdown file").String(),
 }
 
-type EditorView struct {
+type editorView struct {
 	File              string
 	Content           string
 	LineEndings       map[int]string
 	CurrentLineEnding eol.LineEnding
 }
 
-func NewEditorView(filepath string, content string) *EditorView {
-	return &EditorView{
+func newEditorView(filepath string, content string) *editorView {
+	return &editorView{
 		File:        filepath,
 		Content:     content,
 		LineEndings: eol.Descriptions,
@@ -52,56 +51,57 @@ func main() {
 
 	// Prepare (optionally) embedded resources
 	templateBox := rice.MustFindBox("template")
-	staticHTTPBox := rice.MustFindBox("static").HTTPBox()
-	staticServer := http.StripPrefix("/static/", http.FileServer(staticHTTPBox))
+	// staticHTTPBox := rice.MustFindBox("static").HTTPBox()
+	// staticServer := http.StripPrefix("/static/", http.FileServer(staticHTTPBox))
 
 	e := echo.New()
 
 	t := &Template{
 		templates: template.Must(template.New("base").Parse(templateBox.MustString("base.html"))),
 	}
-	e.SetRenderer(t)
+	e.Renderer = t
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/static/*", standard.WrapHandler(staticServer))
+	e.Static("/static/*", "static")
 
 	edit := e.Group("/edit")
-	edit.Get("/*", EditHandler)
-	edit.Post("/*", EditHandlerPost)
+	edit.GET("/*", editHandler)
+	edit.POST("/*", editHandlerPost)
 
-	go WaitForServer()
-	e.Run(standard.New(fmt.Sprintf("127.0.0.1:%d", *args.Port)))
+	go waitForServer()
+	e.Logger.Fatal(e.Start(fmt.Sprintf("%d", *args.Port)))
 }
 
 type Template struct {
 	templates *template.Template
 }
 
+// Render Function
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-func EditHandler(c echo.Context) error {
-	var ev *EditorView
-	ev, ok := c.Get("editorView").(*EditorView)
+func editHandler(c echo.Context) error {
+	var ev *editorView
+	ev, ok := c.Get("editorView").(*editorView)
 	if !ok {
 		log.Println("reading file")
-		filepath := c.P(0)
+		filepath := c.ParamNames()[0]
 		content, err := ioutil.ReadFile(filepath)
 		if err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, "Unable to read requested file")
 		}
-		ev = NewEditorView(filepath, string(content))
+		ev = newEditorView(filepath, string(content))
 		ev.CurrentLineEnding = eol.DetectDefault(ev.Content, eol.OSDefault())
 		log.Println(ev.CurrentLineEnding.Description())
 	}
 	return c.Render(http.StatusOK, "base", ev)
 }
 
-func EditHandlerPost(c echo.Context) error {
-	filepath := c.P(0)
+func editHandlerPost(c echo.Context) error {
+	filepath := c.ParamNames()[0]
 	eolIndex, _ := strconv.Atoi(c.FormValue("eol"))
 	content := c.FormValue("content")
 	convertedContent, err := eol.LineEnding(eolIndex).Apply(content)
@@ -110,11 +110,11 @@ func EditHandlerPost(c echo.Context) error {
 		log.Println("Error while converting EOL. Saving without conversion.")
 	}
 	ioutil.WriteFile(filepath, []byte(convertedContent), 0644)
-	c.Set("editorView", NewEditorView(filepath, content))
-	return EditHandler(c)
+	c.Set("editorView", newEditorView(filepath, content))
+	return editHandler(c)
 }
 
-func WaitForServer() {
+func waitForServer() {
 	log.Printf("Waiting for listener on port %d", *args.Port)
 	url := fmt.Sprintf("http://localhost:%d/edit/%s", *args.Port, url.QueryEscape(*args.File))
 	for {
